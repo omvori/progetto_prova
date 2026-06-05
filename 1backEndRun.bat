0e636aaf72f533d2ef17f02a5c7fe27d52977de7
Andrea <194246510+omvori@users.noreply.github.com>
2026-06-05 12:07:24 +0200

    Update 1backEndRun.bat

--- 1backEndRun.bat
+++ 1backEndRun.bat
@@ -1,11 +1,8 @@
 @echo off
 
-:: 1. Usa CALL. Senza di questo, lo script muore qui.
 call angular_microservice_test\Scripts\activate
 
-:: 2. Ora questo comando verrà eseguito
 cd src\app\backEnd
 
 py server_flask.py
-:: 3. Mantiene il terminale aperto
 cmd /k
