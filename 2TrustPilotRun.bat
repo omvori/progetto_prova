54d14512fd94fe6cff43720927846f6fdc18dd7f
Andrea <194246510+omvori@users.noreply.github.com>
2026-06-05 12:07:13 +0200

    Update 2TrustPilotRun.bat

--- 2TrustPilotRun.bat
+++ 2TrustPilotRun.bat
@@ -1,10 +1,7 @@
 @echo off
 
-:: 1. Usa CALL. Senza di questo, lo script muore qui.
 call angular_microservice_test\Scripts\activate
 
-:: 2. Ora questo comando verrà eseguito
 
 ng serve
-:: 3. Mantiene il terminale aperto
 cmd /k
